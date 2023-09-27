743a3006ef57486d8515161c919e911c19f64a15
chihong33 <60839641+chihong33@users.noreply.github.com>
2023-09-28 00:03:08 +0800

    remvoe test

--- app/View/Components/Home/Intro.php
+++ app/View/Components/Home/Intro.php
@@ -21,7 +21,6 @@ class Intro extends Component
     public function __construct()
     {
         $this->name = "Joseph";
-        dd('test');
         $this->selfie_image = "img/selfie.png";
         $this->introduction = "I am a full-stack web and mobile app developer (Flutter) with 3 years of working experience. I specialize in designing and building user-friendly interfaces and experienced in writing clean, reusable, and maintainable code. I am passionate about staying up-to-date with the latest trends in mobile and web development. Currently, I am actively seeking job opportunities in this field. Feel free to reach out to me if you have a job opportunity that aligns with my skills and interests!";
         $this->resume_link = "/download_resume";
