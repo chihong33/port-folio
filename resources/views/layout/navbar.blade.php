95c5432a7b5e73e6af1a9ec43bf8a7bc041866f3
chihong33 <mokchi2324@hotmail.com>
2023-09-29 17:42:01 +0800

    update z index

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,6 +1,6 @@
 <!-- Navbar section  -->
 <div>
-    <header x-data="{navbarOpen: false}" class="fixed left-0 top-0 z-50 bg-white w-full flex items-center shadow-md dark:bg-slate-900 h-24">
+    <header x-data="{navbarOpen: false}" class="fixed left-0 top-0 z-[10000] bg-white w-full flex items-center shadow-md dark:bg-slate-900 h-24">
         <div class="container">
             <div class="flex -mx-4 items-center justify-between relative">
                 <!-- <div class="pr-4 w-60 max-w-full">
